083a33bf7404ec342cdba3c2e1566c80009fc271
Ben <troazg@gmail.com>
2016-05-11 00:47:26 -0600

    Format pages to center

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples:</h3>
-      <p>Welcome to the Examples Page</p>
+      <h1 className="text-center">Examples:</h1>
+      <p>Here are a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=provo'>Provo, UT</Link>
+        </li>
+        <li>
+          <Link to='/?location=london'>London, UK</Link>
+        </li>
+      </ol>
     </div>
   );
 };
