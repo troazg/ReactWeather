758665e7c6ae23dcd2f8fb3a9a0195b44a6b2b82
Ben <troazg@gmail.com>
2016-05-11 14:25:33 -0600

    Style About Page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,20 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About:</h3>
-      <p>Placeholder for an about section.</p>
+      <h1 className="text-center">About:</h1>
+      <p>This simple weather app was built on React and uses Foudnation
+        for styling.</p>
+      <p>Here you can find some of the tools I used for this project.</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> -
+            The JavaScript framework used.
+        </li>
+        <li>
+          <a href="https://openweathermap.org">Open Weather Map</a> -
+            The Open Weather Map API was used for weather information.
+        </li>
+      </ul>
     </div>
   );
 };
